0551e2dc3e01b4c9b9c15574dab8690c96b711de
Franzé Jr <franzejr@gmail.com>
2019-06-21 15:55:00 -0300

    Add public token

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -17,6 +17,7 @@ class HomeController < ApplicationController
 
   def send_message
     message = UhuraClient::Message.new(
+      public_token: "42c50c442ee3ca01378e",
       receiver_sso_id: "88543898",
       email: UhuraClient::Email.new(
         subject: "Yo!",
@@ -29,7 +30,7 @@ class HomeController < ApplicationController
 
     # create a client by passing the api key, team id and public token
     client = UhuraClient::MessageClient.new(
-      api_key: "b1dcc4b8287a82fe8889", team_id: "1", public_token: "42c50c442ee3ca01378e")
+      api_key: "b1dcc4b8287a82fe8889", team_id: "1", )
     client.send_message(message)
   end
 end
