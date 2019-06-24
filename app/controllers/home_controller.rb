6bd73855183e72ad2da770c3a012d804a2bd0a72
l3x <l3x@users.noreply.github.com>
2019-06-23 21:19:58 -0400

    Use client_id correlate client with message for status
    Note: Clearstream status yet to be tested
    
    - Update home controller
    -- Set email_status and sms_status from params
    -- Add client_id to message instance variable
    -- Call status to update email and sms status after send message
    - Update index (slim) page
    -- Reset window.location to root path
    -- Update email_status and sms_status from instance variables
    -- Fix a few placeholder values
    -- Add <Reset> and <Clear all> buttons

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,20 +4,22 @@ class HomeController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-
+    @email_status = params['email_status']
+    @sms_status = params['sms_status']
   end
 
   def status
+    sleep 1.second
     ## This is the way how we can get the status of a  message
     client = UhuraClient::MessageClient.new(
       api_key: "b1dcc4b8287a82fe8889", team_id: "1", public_token: "42c50c442ee3ca01378e")
 
-    client.status_of(UhuraClient::Message.new(id: "1"))
+    client.status_of(UhuraClient::Message.new(id: @message.client_id))
   end
 
   def send_message
     # Create message with SMS and Email content
-    message = UhuraClient::Message.new(
+    @message = UhuraClient::Message.new(
       public_token: "42c50c442ee3ca01378e",
       receiver_sso_id: params[:receiver_sso_id],
       email_subject: params[:email_subject],
@@ -27,7 +29,8 @@ class HomeController < ApplicationController
             "button": params[:button]
       },
       template_id: params[:template_id],
-      sms_message: params[:sms_message]
+      sms_message: params[:sms_message],
+      client_id: SecureRandom.uuid
       )
 
     client = UhuraClient::MessageClient.new(
@@ -35,11 +38,14 @@ class HomeController < ApplicationController
       team_id: "1"
     )
     begin
-      client.send_message(message)
+      client.send_message(@message)
       flash[:success] =  'Success!'
     rescue StandardError => error
       flash[:error] =  JSON.parse(error.to_s)['error']['message']
     end
-    redirect_to :root
+    email_sms_status = status
+    redirect_to controller: 'home', action: 'index',
+                email_status: email_sms_status['sendgrid_msg_status'],
+                sms_status: email_sms_status['clearstream_msg_status']
   end
 end
